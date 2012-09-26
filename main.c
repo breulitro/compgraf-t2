985f299c82e4f0e03d84f129c3bc6ba0f1bcdc54
Cristiano Fernandes <cristianobfernandes@gmail.com>
2012-09-26 18:44:52 -0300

    Coding standard.
    Signed-off-by: Cristiano Fernandes <cristianobfernandes@gmail.com>

--- main.c
+++ main.c
@@ -8,64 +8,71 @@
 
 // all variables initialized to 1.0, meaning
 // the triangle will initially be white
-float red=1.0f, blue=1.0f, green=1.0f;
+float red = 1.0f, blue = 1.0f, green = 1.0f;
 float angle = 0.0;
 
 void changeSize(int w, int h) {
+	float ratio;
 
-  // Prevent a divide by zero, when window is too short
-  // (you cant make a window of zero width).
-  if(h == 0)
-    h = 1;
-  float ratio = 1.0* w / h;
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if (h == 0)
+		h = 1;
 
-  // Use the Projection Matrix
-  glMatrixMode(GL_PROJECTION);
+	ratio = 1.0 * w / h;
 
-        // Reset Matrix
-  glLoadIdentity();
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
 
-  // Set the viewport to be the entire window
-  glViewport(0, 0, w, h);
+	// Reset Matrix
+	glLoadIdentity();
 
-  gluPerspective(45,ratio,1,1000);
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
 
-  // Get Back to the Modelview
-  glMatrixMode(GL_MODELVIEW);
+	gluPerspective(45, ratio, 1, 1000);
+
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void processNormalKeys(unsigned char key, int x, int y) {
-
-  if (key == 27)
-    exit(0);
+	if (key == 27)
+		exit(0);
 }
 
 void processSpecialKeys(int key, int x, int y) {
+	int mod;
 
-  int mod;
-  switch(key) {
-    case GLUT_KEY_F1 :
-       mod = glutGetModifiers();
-       if (mod == (GLUT_ACTIVE_CTRL|GLUT_ACTIVE_ALT)) {
-        red = 1.0; green = 1.0; blue = 1.0;
-       }
-       red = 1.0; green = 0.0; blue = 0.0;
-       break;
-    case GLUT_KEY_F2 :
-       red = 0.0;
-       green = 1.0;
-       blue = 0.0; break;
-    case GLUT_KEY_F3 :
-       red = 0.0;
-       green = 0.0;
-       blue = 1.0; break;
-    case GLUT_KEY_LEFT:
-      angle += .5;
-      break;
-    case GLUT_KEY_RIGHT:
-      angle -= .5;
-      break;
-  }
+	switch(key) {
+		case GLUT_KEY_F1 :
+			mod = glutGetModifiers();
+			if (mod == (GLUT_ACTIVE_CTRL | GLUT_ACTIVE_ALT)) {
+				red = 1.0;
+				green = 1.0;
+				blue = 1.0;
+			}
+			red = 1.0;
+			green = 0.0;
+			blue = 0.0;
+			break;
+		case GLUT_KEY_F2 :
+			red = 0.0;
+			green = 1.0;
+			blue = 0.0;
+			break;
+		case GLUT_KEY_F3 :
+			red = 0.0;
+			green = 0.0;
+			blue = 1.0;
+			break;
+		case GLUT_KEY_LEFT:
+			angle += .5;
+			break;
+		case GLUT_KEY_RIGHT:
+			angle -= .5;
+			break;
+	}
 }
 
 GSList *interpolate(GSList *l) {
@@ -73,83 +80,82 @@ GSList *interpolate(GSList *l) {
 }
 
 void desenhaChao() {
-	glColor3f(0,0,1);
+	float z, x;
+
+	glColor3f(0, 0, 1);
 	glLineWidth(1);
 	glBegin(GL_LINES);
-	for(float z=-1000; z<=1000; z+=10)
-	{
-		glVertex3f(-1000,-0.1f,z);
-		glVertex3f( 1000,-0.1f,z);
+
+	for(z =- 1000; z <= 1000; z += 10) {
+		glVertex3f(-1000, -0.1f, z);
+		glVertex3f(1000, -0.1f, z);
 	}
-	for(float x=-1000; x<=1000; x+=10)
-	{
-		glVertex3f(x,-0.1f,-1000);
-		glVertex3f(x,-0.1f,1000);
+	for(x =- 1000; x <= 1000; x += 10) {
+		glVertex3f(x, -0.1f, -1000);
+		glVertex3f(x, -0.1f, 1000);
 	}
 	glEnd();
 	glLineWidth(1);
 }
 
 void renderScene(void) {
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	desenhaChao();
 
-  // Reset transformations
-  glLoadIdentity();
-  // Set the camera
-  gluLookAt(  0.0f, 0.0f, 10.0f,
-      0.0f, 0.0f,  0.0f,
-      0.0f, 1.0f,  0.0f);
+	// Reset transformations
+	glLoadIdentity();
 
-  glRotatef(angle, 0.0f, 1.0f, 0.0f);
+	// Set the camera
+	gluLookAt(0.0f, 0.0f, 10.0f, 0.0f, 0.0f,  0.0f, 0.0f, 1.0f, 0.0f);
 
-  glColor3f(red, green, blue);
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
 
-  glBegin(GL_TRIANGLES);
-    glVertex3f(-1.0f,-2.0f, 0.0f);
-    glVertex3f( 2.0f, 0.0f, 0.0);
-    glVertex3f( 0.0f, 2.0f, 0.0);
-  glEnd();
+	glColor3f(red, green, blue);
 
-  glutSwapBuffers();
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-1.0f,-2.0f, 0.0f);
+	glVertex3f( 2.0f, 0.0f, 0.0);
+	glVertex3f( 0.0f, 2.0f, 0.0);
+	glEnd();
+
+	glutSwapBuffers();
 }
 
 int main(int argc, char **argv) {
 
-  if (argc < 2) {
-    printf("usage: %s <script file> [glut params]\n", argv[0]);
-    return 1;
-  }
+	if (argc < 2) {
+		printf("usage: %s <script file> [glut params]\n", argv[0]);
+		return 1;
+	}
 
-  GSList *actors = read_script(argv[1]);
-  //FIXME: argv[0] que vai pro glutInit não é o nome do programa
-  argv++;
-  argc--;
-  printf("script loaded\n");
+	GSList *actors = read_script(argv[1]);
+	//FIXME: argv[0] que vai pro glutInit não é o nome do programa
+	argv++;
+	argc--;
+	printf("script loaded\n");
 
-  dump_actors();
-  GHashTable *obj = read_obj("yoda.obj");
-  dump_hash(obj);
+	dump_actors();
+	GHashTable *obj = read_obj("yoda.obj");
+	dump_hash(obj);
 
-//	g_hash_table_foreach(obj, 
-//  l = interpolate(l);
+	//	g_hash_table_foreach(obj, 
+	//  l = interpolate(l);
 	//GLModel *glm = glmReadObj("yoda.obj");
-  // init GLUT and create window
-  glutInit(&argc, argv);
-  //-1 == default
-  glutInitWindowPosition(100, 100);
-  glutInitWindowSize(800, 600);
-  glutInitDisplayMode(GLUT_RGB|GLUT_DEPTH);
-  glutCreateWindow("CG T2");
+	// init GLUT and create window
+	glutInit(&argc, argv);
+	//-1 == default
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutInitDisplayMode(GLUT_RGB|GLUT_DEPTH);
+	glutCreateWindow("CG T2");
 
-  // register callbacks
-  glutDisplayFunc(renderScene);
-  glutReshapeFunc(changeSize);
-  glutIdleFunc(renderScene);
-  glutKeyboardFunc(processNormalKeys);
-  glutSpecialFunc(processSpecialKeys);
+	// register callbacks
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
+	glutKeyboardFunc(processNormalKeys);
+	glutSpecialFunc(processSpecialKeys);
 
-  // enter GLUT event processing cycle
-  glutMainLoop();
+	// enter GLUT event processing cycle
+	glutMainLoop();
 }
